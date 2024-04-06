fad4087786e4ced30960280525e4cb1499bc21fb
zhangyibing <zhangybchn@163.com>
2024-04-06 13:53:05 +0800

    version 1

--- app.py
+++ app.py
@@ -5,3 +5,17 @@ import pickle
 flask_app = Flask(__name__)
 model = pickle.load(open("finalized_model.pkl", "rb"))
 
+@flask_app.route("/")
+def home():
+    return render_template("index.html")
+
+@flask_app.route("/predict", methods = ["POST"])
+def predict():
+    float_features = [float(x) for x in request.form.values()]
+    features = [np.array(float_features)]
+    prediction = model.predict(features)
+
+    return render_template("index.html", prediction_text = "the result is {}".format(prediction))
+
+if __name__ == "__main__":
+    flask_app.run(debug=True)
